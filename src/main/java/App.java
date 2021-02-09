ff160b0761a7b850ed4b37109256259829a24479
Ruslan <r.aleroev@gmail.com>
2021-02-09 15:49:11 +0300

    1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,20 +9,14 @@ public class App {
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
 
-        ApplicationContext applicationContext1 =
-                new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean1.getMessage());
 
-        ApplicationContext applicationContext2 =
-                new AnnotationConfigApplicationContext(AppConfig.class);
         Cat bean2 =
                 (Cat) applicationContext.getBean("cat");
         System.out.println(bean2.getName());
 
-        ApplicationContext applicationContext3 =
-                new AnnotationConfigApplicationContext(AppConfig.class);
         Cat bean3 =
                 (Cat) applicationContext.getBean("cat");
         System.out.println(bean3.getName());
